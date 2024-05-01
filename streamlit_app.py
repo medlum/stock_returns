02ce88424d81c04a0d31f0398c608fe94a3e6af4
medlum <andy_oh18@yahoo.com>
2024-05-01 13:58:16 +0800

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
+import altair as alt
+
+description = """
+Select at least 2 counters to compare their total gain over 10 years.
+"""
+
+st.header('Total Relative Return')
+
+st.write(description)
+
+stock_df = pd.read_csv('resources/sg_stocks_list.csv')
+# stock_df.index = stock_df['Symbol']
+stock_name = stock_df['Name']
+
+options = st.multiselect('Your selection', stock_name)
+filter = stock_df['Name'].isin(options)
+selected_symbols = stock_df[filter]['Symbol'].to_list()
+
+if len(selected_symbols) > 1:
+    data = yf.download(selected_symbols, period='10y')
+    data_closePrice = data['Adj Close']
+
+    returns = data_closePrice.pct_change(1)
+    log_returns = np.log(data_closePrice).diff()
+    log_returns = log_returns.set_axis(options, axis=1)
+    trr = 100*(np.exp(log_returns.cumsum()) - 1)
+
+    trr_melt = trr.reset_index().melt(
+        id_vars='Date',
+        var_name='Counter',
+        value_name='Total Relative Return (%)'
+    )
+
+    line = alt.Chart(trr_melt).mark_line().encode(
+        x='Date',
+        y='Total Relative Return (%)',
+        color='Counter').properties(
+        width=800,
+        height=500
+    ).interactive()
+    st.altair_chart(line)
